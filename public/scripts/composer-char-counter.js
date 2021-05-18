a1bd36ef630d7487899c8aaff2b4bb8e25c00ed1
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-18 17:45:21 -0400

    counter now turning red if chars goes over the limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("Connected")
-  $('#tweet-text').on("input", function(e){
-    let $count = 140;
+  $('#tweet-text').on("input", function (e) {
+    let $charLimit = 140;
     let $inputLength = $(this).val().length;
-    let $charRemaining = $count - $inputLength; 
+    let $charRemaining = $charLimit - $inputLength;
+    // Traversing to the parent element of the textarea
     const $parent = ($(this).parent())
     let $counterElem = $parent.find('.counter');
+    // setting counter elements value to the remaining chars
     $counterElem.val($charRemaining);
+    if ($inputLength > $charLimit) {
+      $counterElem.addClass("over-limit");
+    } else {
+      $counterElem.removeClass("over-limit")
+    }
   })
 });
