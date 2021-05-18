65fc0f2475b349f38c54a0a5d10f08c11e31574e
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-18 17:35:55 -0400

    counter value now reflecting textarea input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   console.log("Connected")
-  const tweet = $('#tweet-text');
-  tweet.on("keypress", function(e){
-    console.log($(this).val());
+  $('#tweet-text').on("input", function(e){
+    let $count = 140;
+    let $inputLength = $(this).val().length;
+    let $charRemaining = $count - $inputLength; 
+    const $parent = ($(this).parent())
+    let $counterElem = $parent.find('.counter');
+    $counterElem.val($charRemaining);
   })
 });
