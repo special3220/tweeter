73e9a51e3ec76b69936198ad56ab488be5b5112b
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-19 15:20:02 -0400

    jquery post and get method implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,46 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function (tweet) {
   // creates and return HTML for each tweet
-  return $(`<article class="tweet">
+  const $tweet = $(`<article class="tweet">
                       <header>
                         <div>
                           <img src=${tweet.user.avatars} alt="profile avatar">
                           <span>${tweet.user.name}</span>
                         </div>
-                        <div>
-                          <span>${tweet.user.handle}</span>
-                        </div>
+                        <div><span>${tweet.user.handle}</span></div>
                       </header>
                       <p>${tweet.content.text}</p>
                       <footer>
-                        <div><span>10</span> Days Ago</div>
+                        <div><span>${timeago.format(tweet.created_at)}</span></div>
                         <div>
                           <i class="fas fa-flag"></i>
                           <i class="fas fa-retweet"></i>
@@ -51,18 +24,38 @@ const createTweetElement = function (tweet) {
                         </div>
                       </footer>
                     </article>`);
+  return $tweet;
 }
 
 const renderTweets = function(tweets) {
   // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for(let tweet of tweets){
-    $('#tweets-container').append(createTweetElement(tweet));
+  for(const tweet of tweets){
+    // calling createTweetElement to create tweet element
+    const tweetElem = createTweetElement(tweet);
+    // Appending tweet element to the tweet container
+    $('#tweets-container').prepend(tweetElem);
   }
 }
-
+const loadTweets = function() {
+  $.get('/tweets', function(data) {
+    renderTweets(data)
+  })
+}
 
 $(document).ready(function () {
-  renderTweets(data)
+  loadTweets();
+  $("#tweet-form").submit(function(e){
+    e.preventDefault();
+    const $form = $(this);
+    const tweetText = $form.find("textarea").val();
+    const url = $form.attr('action');
+    console.log($form.serialize());
+    let posting = $.post(url, $form.serialize())
+
+    posting.done(function(){
+      $('#tweets-container').empty();
+      loadTweets();
+    })
+    
+  })
 })
