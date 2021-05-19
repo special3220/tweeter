199e280cbb721a45a89afe07a0b56452c47cae86
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-19 14:04:08 -0400

    implemented appending of tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,33 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-function createTweetElement(tweet) {
+const createTweetElement = function (tweet) {
+  // creates and return HTML for each tweet
   return $(`<article class="tweet">
                       <header>
                         <div>
@@ -39,9 +53,16 @@ function createTweetElement(tweet) {
                     </article>`);
 }
 
-const $tweet = createTweetElement(tweetData)
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for(let tweet of tweets){
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+}
+
+
 $(document).ready(function () {
-  // $('main').append($tweet);
-  for(let i = 0; i< 5; i++) {
-    $('#tweets-container').append($tweet);  }
+  renderTweets(data)
 })
